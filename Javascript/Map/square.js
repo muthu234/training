70a2ef20be7d58e3bb2822c5739d9c099500222c
MUTHUVEL <142568047+muthu234@users.noreply.github.com>
2023-09-07 15:46:33 +0530

    Add files via upload

---
+++ Javascript/Map/square.js
@@ -0,0 +1,5 @@
+// 1.Write a function that takes an array of numbers and returns a new array with each number squared. Use the Map object to accomplish this.
+a=[5,6,7,8]
+m=a.map(e=>e**2)
+console.log(a);
+console.log(m);
