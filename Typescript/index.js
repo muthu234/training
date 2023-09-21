0cfc511b54d9ebb947d96fcfcd446513fb53f0f2
muthu234 <muthu638381@gmail.com>
2023-09-21 12:15:08 +0530

    interface, get & set method

--- Typescript/index.js
+++ Typescript/index.js
@@ -45,34 +45,34 @@
 // b=a;
 // console.log(b);
 // -----------------------------------------
-// now for proper way to TS
-// Type annotation
+//------------- now for proper way to TS
+// ---------Type annotation
 // var a:number;
-// Type annotation setting the value
+// -----------Type annotation setting the value
 // var b:number=10;
-//  Type inference(number)
+// ------------ Type inference(number)
 // var c=10;
-// Type inference(number)
+// --------------Type inference(number)
 // var d=c+10;
-// Type inference(string)
+// -------------Type inference(string)
 // var s=b+'hello World!';
 // var nottype:number=b+'hello World!';
-// primitive Types
-// 1.explict Type annotation
+// -------primitive Types
+// ---1.explict Type annotation
 // let firstname:string ='muthuvel';
 // console.log(firstname);
-// 2.implict Type annotation
+// ----2.implict Type annotation
 // let firstname='muthuvel';
 // console.log(firstname);
-// // union Type
+//-------------- union Type
 // let age:number|string;
 // age='helo';
 // age=20;
 // console.log(age);
-// dynamic Type
+// ------dynamic Type
 // let age:any=3.5;
 // console.log(age);
-//create object
+//------create object
 // let a:{
 //     name:string;
 //     age:number;
@@ -87,7 +87,7 @@
 // a.push('hello','world');
 // console.log(a);
 // console.log(a[0]);
-// create array of object
+// --------------create array of object
 // var a:{
 //     name:string,
 //     age:number //type annotation
@@ -100,7 +100,7 @@
 //     name:'sasti',age:24 
 // }];
 // console.log(a[0].name,a[1].age);
-// fliter by using array of object
+// ----------------fliter by using array of object
 // var fil=a.filter(e=>
 //     {
 //         if (e.age >20)
@@ -117,13 +117,13 @@
 //     console.log(e.name);
 // })
 // console.log(fil2[0].name);
-// normal function with return  datatype
+// ------------normal function with return  datatype
 // let a:number,b:number;
 // function fn(a:number=10,b:number=30):number{
 //     return a+b 
 // }
 // console.log(fn(10,20));
-// optional argument using function
+// -----optional argument using function
 // function fn(a:number,b:number,c?:number):number
 // {
 //     if (typeof c!='undefined')
@@ -133,7 +133,7 @@
 //     return a*b
 // }
 // console.log(fn(10,2));
-// optional argument using  arrow function & return Type
+// ----optional argument using  arrow function & return Type
 // var a=(a:number,b:number,c?:number):number =>
 // {
 //         if (typeof c!='undefined')
@@ -143,18 +143,69 @@
 //         return a*b
 //     }
 // console.log(a(10,2));
-// default value using function & return Type
+// ----default value using function & return Type
 // function val(a:number,b:number,c:number):number{
 //     return a+b+c
 // }
 // console.log(val(2,2,3));
-// dynamic number of parameter (...) => to use multiple values
+// ---dynamic number of parameter (...) => to use multiple values
 // function fn(a:number,b:number,...rest:number[]):number{
 //     return a+b+rest.reduce((p,c)=> p+c,-1)
 // }
 // console.log(fn(3,3,3,3,3,3));
-// annoymus function
-var fn = function (a, b, c) {
-    return a / b * c;
-};
-console.log(fn(10, 2, 2));
+// ----annoymus function
+// var fn=function(a:number,b:number,c:number):number
+// {
+//     return a/b*c
+// }
+// console.log(fn(10,2,2));
+// ---using unknown datatype
+// let a:unknown='hello'
+// let b=(a as string).length;
+// console.log(b);
+// ---- declare array:
+// let names:(string | number)[]=[5,6,'Hello'];
+// console.log(names);
+// --------------create array of object 
+// var obj:{name:string,age:number,gender:string}[]=[{name:'muthu',age:20,gender:'Male'}];
+// console.log(obj[0].name);
+// --------------21.9.2023
+//  class myclass{         ------interface
+//     // a!:number; assumption operator
+//     a!:number;
+//     b!:number;
+//     // readonly c:number=3;read only property. it is used in only read
+//     constructor(){
+//         // this.a=1;
+//         //  var c=20;
+//     }
+// }
+//  let o=new myclass()
+//  o.a=10;
+//  o.b=20;
+// o.c=20;//readonly error
+// console.log(o.a);
+// console.log(o.b);
+// console.log(o.c);
+// ---  access private variable(get and set) method --private variable assign => _variable name(ex=>_a)
+var myclass = /** @class */ (function () {
+    function myclass() {
+        this._val = "20";
+    }
+    Object.defineProperty(myclass.prototype, "val", {
+        get: function () {
+            console.log("hello");
+            return this._val;
+        },
+        set: function (a) {
+            this._val = a;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return myclass;
+}());
+var o = new myclass();
+var a = "50";
+console.log(o.val);
+console.log(a);
--- Typescript/index.ts
+++ Typescript/index.ts
@@ -47,34 +47,34 @@
 // b=a;
 // console.log(b);
 // -----------------------------------------
-// now for proper way to TS
-// Type annotation
+//------------- now for proper way to TS
+// ---------Type annotation
 // var a:number;
-// Type annotation setting the value
+// -----------Type annotation setting the value
 // var b:number=10;
-//  Type inference(number)
+// ------------ Type inference(number)
 // var c=10;
-// Type inference(number)
+// --------------Type inference(number)
 // var d=c+10;
-// Type inference(string)
+// -------------Type inference(string)
 // var s=b+'hello World!';
 // var nottype:number=b+'hello World!';
-// primitive Types
-// 1.explict Type annotation
+// -------primitive Types
+// ---1.explict Type annotation
 // let firstname:string ='muthuvel';
 // console.log(firstname);
-// 2.implict Type annotation
+// ----2.implict Type annotation
 // let firstname='muthuvel';
 // console.log(firstname);
-// // union Type
+//-------------- union Type
 // let age:number|string;
 // age='helo';
 // age=20;
 // console.log(age);
-// dynamic Type
+// ------dynamic Type
 // let age:any=3.5;
 // console.log(age);
-//create object
+//------create object
 // let a:{
 //     name:string;
 //     age:number;
@@ -89,7 +89,7 @@
 // a.push('hello','world');
 // console.log(a);
 // console.log(a[0]);
-// create array of object
+// --------------create array of object
 // var a:{
 //     name:string,
 //     age:number //type annotation
@@ -104,7 +104,7 @@
     
 // }];
 // console.log(a[0].name,a[1].age);
-// fliter by using array of object
+// ----------------fliter by using array of object
 // var fil=a.filter(e=>
 //     {
 //         if (e.age >20)
@@ -123,13 +123,13 @@
 //     console.log(e.name);
 // })
 // console.log(fil2[0].name);
-// normal function with return  datatype
+// ------------normal function with return  datatype
 // let a:number,b:number;
 // function fn(a:number=10,b:number=30):number{
 //     return a+b 
 // }
 // console.log(fn(10,20));
-// optional argument using function
+// -----optional argument using function
 // function fn(a:number,b:number,c?:number):number
 // {
 //     if (typeof c!='undefined')
@@ -139,7 +139,7 @@
 //     return a*b
 // }
 // console.log(fn(10,2));
-// optional argument using  arrow function & return Type
+// ----optional argument using  arrow function & return Type
 // var a=(a:number,b:number,c?:number):number =>
 // {
 //         if (typeof c!='undefined')
@@ -149,22 +149,72 @@
 //         return a*b
 //     }
 // console.log(a(10,2));
-// default value using function & return Type
+// ----default value using function & return Type
 // function val(a:number,b:number,c:number):number{
 //     return a+b+c
 // }
 // console.log(val(2,2,3));
-// dynamic number of parameter (...) => to use multiple values
+// ---dynamic number of parameter (...) => to use multiple values
 // function fn(a:number,b:number,...rest:number[]):number{
 //     return a+b+rest.reduce((p,c)=> p+c,-1)
 // }
 // console.log(fn(3,3,3,3,3,3));
-// annoymus function
-var fn=function(a:number,b:number,c:number):number
-{
-    return a/b*c
+// ----annoymus function
+// var fn=function(a:number,b:number,c:number):number
+// {
+//     return a/b*c
+// }
+// console.log(fn(10,2,2));
+// ---using unknown datatype
+// let a:unknown='hello'
+// let b=(a as string).length;
+// console.log(b);
+// ---- declare array:
+// let names:(string | number)[]=[5,6,'Hello'];
+// console.log(names);
+// --------------create array of object 
+// var obj:{name:string,age:number,gender:string}[]=[{name:'muthu',age:20,gender:'Male'}];
+// console.log(obj[0].name);
+// --------------21.9.2023
+//  class myclass{         ------interface
+//     // a!:number; assumption operator
+//     a!:number;
+//     b!:number;
+//     // readonly c:number=3;read only property. it is used in only read
+//     constructor(){
+//         // this.a=1;
+//         //  var c=20;
+//     }
+// }
+//  let o=new myclass()
+//  o.a=10;
+//  o.b=20;
+// o.c=20;//readonly error
+// console.log(o.a);
+// console.log(o.b);
+// console.log(o.c);
+// ---  access private variable(get and set) method --private variable assign => _variable name(ex=>_a)
+class myclass{
+    _val:string="20"
+    get val():string{
+        // console.log("hello");
+        return this._val
+    }
+    set val(a:string){
+         this._val=a;
 }
-console.log(fn(10,2,2));
+}
+let o=new myclass()
+ let a="50"
+console.log(o.val);
+console.log(a);
+
+
+    
+
+
+
+
 
 
 
