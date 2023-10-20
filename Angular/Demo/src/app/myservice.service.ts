99f1ca71e426d5eb2646e0eae091012d372bb871
muthu234 <muthu638381@gmail.com>
2023-10-20 16:08:54 +0530

    Angular practice

--- Angular/Demo/src/app/myservice.service.ts
+++ Angular/Demo/src/app/myservice.service.ts
@@ -4,18 +4,21 @@ import { HttpClient } from '@angular/common/http';//step:1
 @Injectable({providedIn: 'root'})
 export class ServiceNameService {
   constructor(private httpClient: HttpClient) { }
-  
-}
-
+ }
 @Injectable({
   providedIn: 'root'
 })
 export class MyserviceService {
-  apiUrl:string ='https://jsonplaceholder.typicode.com/todos/'//step 3
+  apiUrl:string ='https://jsonplaceholder.typicode.com/todos/1'//step 3
+  Url:string ='http://localhost:3008'
 
   constructor(private http:HttpClient) { }//step:2
 getToDos(){
   return this.http.get(this.apiUrl);//step:4
+}
+getAll()
+{
+  return this.http.get(this.Url+'/getAllUsers')
 }
   getStudents(){
   return [
@@ -24,6 +27,5 @@ getToDos(){
   {name:'mani',age:24,RegNO:21504303},
   {name:'vel',age:22,RegNO:21504304},
 ];
-
   } 
 }
--- Angular/Demo/src/app/student/student.component.ts
+++ Angular/Demo/src/app/student/student.component.ts
@@ -9,8 +9,13 @@ import { MyserviceService } from '../myservice.service';
 export class StudentComponent implements OnInit {
   student:any[]=[];
   toDo:any[]=[]; //step 5:1
+  a:any;
   constructor(private myservice:MyserviceService){}
   ngOnInit(): void {
+    this.myservice.getAll().subscribe((data)=>
+    {
+        this.a=data
+    })
     this.student=this.myservice.getStudents();
     this.myservice.getToDos().subscribe((data:any) =>//step:5:2
     {
