7e52ef63719ff701922858ae24e9f95440f9a341
muthu234 <muthu638381@gmail.com>
2023-10-27 13:03:31 +0530

    crud operation completed

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const cors= require("cors")
+const cors = require("cors")
 // instead of using body parser we can use this
 app.use(express.json())
 app.use(cors())
@@ -18,20 +18,21 @@ return;
 }
 console.log("connected as id " + db.threadId);
 });
-// app.post("/createUser", (req, res) => {db.query(
-// "insert into userdata (userName,email,message,isActive) values(?,?,?)",
-// [req.body.name, req.body.email,req.body.message,1],
-// function (error, results, fields) {
-// if (error) console.log(error);
-// console.log("Created successfully");
-// res.json(results);
-// }
-// );
-// });
+//create user
+app.post("/createUser", (req, res) => {db.query(
+"insert into studentdetails (regno,name,age,department,isactive) values(?,?,?,?,?)",
+[req.body.regno, req.body.name,req.body.age,req.body.department,1],
+function (error, results, fields) {
+if (error) console.log(error);
+console.log("Created successfully");
+res.json(results);
+}
+);
+});
 // get all users
 app.get("/getAllUsers",(req, res) => {
 db.query(
-"SELECT id,RegNo,Name,Department from Studentdetails where is_active=1 ",
+"SELECT regno,name,age,department from studentdetails where isactive=1 ",
 function (error, results, fields) {
 if (error) console.log(error);
 console.log("All Users:", results);
@@ -40,39 +41,39 @@ res.json(results);
 );
 });
 // // get user by id++++
-// app.get("/getUserById/:id", (req, res) => {
-// // let user = usersList.filter(e=>e.id==req.params.id)
-// // res.json(user)
-// db.query("SELECT * from userdata where id=?",
-// [req.params.id],
-// function (error, results, fields) {
-// if (error) console.log(error);
-// console.log("one user:", results);
-// res.json(results);
-// }
-// );
-// });
-// app.put("/updateUserById/:id", (req, res) => {
-// db.query(
-// "update userdata set userName=?,email=?, message=? where id=?",
-// [req.body.name,req.body.email,req.body.message, req.params.id],
-// function (error, results, fields) {
-// if (error) console.log(error);
-// console.log("updated successfully");
-// res.json(results);
-// }
-// );
-// });
-// app.put("/deleteUserById/:id", (req, res) => {
-// db.query(
-// "update userdata set isActive=0 where id=?",
-// [req.params.id],
-// function (error, results, fields) {
-// if (error) console.log(error);console.log("updated successfully");
-// res.json(results);
-// }
-// );
-// });
-app.listen(3008, () => {
-console.log("listening on port 3008");
+app.get("/getUserById/:regno", (req, res) => {
+// let user = usersList.filter(e=>e.id==req.params.id)
+// res.json(user)
+db.query("SELECT * from studentdetails where regno=?",
+[req.params.regno],
+function (error, results, fields) {
+if (error) console.log(error);
+console.log("one user:", results);
+res.json(results);
+}
+);
+});
+app.put("/updateUserById/:regno", (req, res) => {
+db.query(
+"update studentdetails set name=?,age=?, department=? where regno=?",
+[req.body.name,req.body.age,req.body.department, req.params.regno],
+function (error, results, fields) {
+if (error) console.log(error);
+console.log("updated successfully");
+res.json(results);
+}
+);
+});
+app.put("/deleteUserById/:regno", (req, res) => {
+db.query(
+"update studentdetails set isActive=0 where regno=?",
+[req.params.regno],
+function (error, results, fields) {
+if (error) console.log(error);console.log("updated successfully");
+res.json(results);
+}
+);
+});
+app.listen(3000, () => {
+console.log("listening on port 3000");
 });
