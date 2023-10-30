cf842ef8dd222fbad68242f903c6095f33fcb184
muthu234 <muthu638381@gmail.com>
2023-10-30 10:37:32 +0530

    Form Validation

--- Angular/Demo/src/app/form/form.component.ts
+++ Angular/Demo/src/app/form/form.component.ts
@@ -9,11 +9,18 @@ import { Component } from '@angular/core';
 export class FormComponent {
  user=new User('','','','',false)
  a:boolean=false;
+//  c:boolean=false;
  sub(){
   console.log(this.user);
  }
  Validate(){
   this.a=true;
+ }
+ cancel(){
+// this.c=true;
+location.reload();
+alert("Are you exit this form");
+// history.go(0);  
  }
  
 }
