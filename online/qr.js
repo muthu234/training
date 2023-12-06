1109bdd177ba00a2fa2a2fa664ee5d51a5259b59
muthu234 <muthuvel638381@gmail.com>
2023-12-06 16:10:59 +0530

    qrcode completed

---
+++ online/qr.js
@@ -0,0 +1,13 @@
+// function generateQRCode()
+// {
+//     var url=document.getElementById("UrlInput").value;
+//     var qrcodeDiv=document.getElementById("qrcode");
+//     //clear new QR code
+//     qrcodeDiv.innerHTML="test";
+//     //Generate New QR code
+//     var qrcode= new QRcode(qrcodeDiv,{
+//         text:url,
+//         width:128,
+//         Height:128
+//     });
+// }
